8d1d209ec592176d5b6d25281a10328586d8b543
beauchama <abeauchamp@pulsewave.dev>
2023-09-20 19:46:31 -0400

    Added FlagQuest project and needed files

---
+++ readme.md
@@ -0,0 +1,3 @@
+# ![logo](./assets/icons/icon-32.png) [Flag Quest](flag-quest.beauchama.com)
+
+The goal is to guess the flag of a country and its neighboring flags. This game is made in C# using Blazor WebAssembly.
---
+++ src/FlagQuest.Web/Program.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+using FlagQuest.Web;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+await builder.Build().RunAsync().ConfigureAwait(false);
