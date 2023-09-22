2455b3b70826c671049f44000f090d123b5466b8
beauchama <abeauchamp@pulsewave.dev>
2023-09-22 16:51:47 -0400

    Added LanguagesJsonConverter and tests

---
+++ src/FlagQuest.CountryConverter/Converters/LanguagesJsonConverter.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FlagQuest.CountryConverter.Converters;
+
+internal sealed class LanguagesJsonConverter : JsonConverter<IEnumerable<string>>
+{
+    public override IEnumerable<string>? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        List<string> languages = [];
+
+        if (reader.TokenType != JsonTokenType.StartObject)
+            return languages;
+
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndObject)
+                break;
+
+            if (reader.TokenType == JsonTokenType.PropertyName)
+                languages.Add(reader.GetString()!);
+        }
+
+        return languages;
+    }
+
+    public override void Write(Utf8JsonWriter writer, IEnumerable<string> value, JsonSerializerOptions options) => throw new NotSupportedException();
+}
