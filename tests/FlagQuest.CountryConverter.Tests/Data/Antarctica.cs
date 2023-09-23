fa4928d44f542fbff94920361bfd80ac6e2475d5
beauchama <abeauchamp@pulsewave.dev>
2023-09-22 21:52:10 -0400

    Added CountryJsonFixture

---
+++ tests/FlagQuest.CountryConverter.Tests/Data/Antarctica.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace FlagQuest.CountryConverter.Data;
+
+public sealed record Antarctica : ICountryData
+{
+    public string Common { get; } = "Antarctica";
+
+    public string Official { get; } = "Antarctica";
+
+    public string CountryCode3 { get; } = "ATA";
+
+    public IEnumerable<string> Capitals { get; } = [];
+
+    public string Region { get; } = "Antarctic";
+
+    public string SubRegion { get; } = string.Empty;
+
+    public IEnumerable<string> Languages { get; } = [];
+
+    public string FrenchCommon { get; } = "Antarctique";
+
+    public string FrenchOfficial { get; } = "Antarctique";
+
+    public double Latitude { get; } = -90;
+
+    public double Longitude { get; }
+
+    public bool Landlocked { get; }
+
+    public IEnumerable<string> Borders { get; } = [];
+
+    public double Area { get; } = 14000000;
+}
---
+++ tests/FlagQuest.CountryConverter.Tests/Fixtures/CountryJsonFixture.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+using System.Text.Json;
+
+namespace FlagQuest.CountryConverter.Fixtures;
+
+public class CountryJsonFixture
+{
+    private readonly Dictionary<string, byte[]> _jsons;
+    private readonly JsonSerializerOptions _options = new()
+    {
+        PropertyNameCaseInsensitive = true,
+    };
+
+    public CountryJsonFixture()
+    {
+        _jsons = Directory
+            .GetFiles("Data")
+            .ToDictionary(f => Path.GetFileNameWithoutExtension(f)!, File.ReadAllBytes);
+    }
+
+    public T Deserialize<T>(string countryName)
+        => JsonSerializer.Deserialize<T>(_jsons[countryName], _options)!;
+}
