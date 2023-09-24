9d495dc4859ca514138923852791cfa4afb8b8a8
beauchama <abeauchamp@pulsewave.dev>
2023-09-24 15:06:36 -0400

    Refact projects for a better structure Pt.1

--- src/FlagQuest.CountryConverter/Resources/Translation.cs
+++
@@ -1,6 +0,0 @@
-﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
-// The source code is licensed under MIT License.
-
-namespace FlagQuest.CountryConverter.Resources;
-
-internal sealed record Translation(string Common, string Official);
