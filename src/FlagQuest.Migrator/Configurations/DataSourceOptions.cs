4c45b321aa1b8f7d9ed6dd41ea94db5b9e29d9ac
beauchama <abeauchamp@pulsewave.dev>
2023-09-24 17:32:22 -0400

    Added appsettings

---
+++ src/FlagQuest.Migrator/Configurations/DataSourceOptions.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace FlagQuest.Migrator.Configurations;
+
+internal sealed class DataSourceOptions
+{
+    public const string Section = "sources";
+
+    public required Uri Countries { get; set; }
+}
--- src/FlagQuest.Migrator/Program.cs
+++ src/FlagQuest.Migrator/Program.cs
@@ -1,6 +1,14 @@
 ﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
 // The source code is licensed under MIT License.
 
-string helloWorld = "Hello, world!";
+using FlagQuest.Migrator.Configurations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-Console.WriteLine(helloWorld);
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services
+    .Configure<DataSourceOptions>(builder.Configuration.GetSection(DataSourceOptions.Section))
+    .Configure<JsonOptions>(builder.Configuration.GetSection(JsonOptions.Section));
+
+_ = builder.Build();
