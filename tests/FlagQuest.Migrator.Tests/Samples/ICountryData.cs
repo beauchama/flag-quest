bd6fc9da7538c3d21b2abea92c14b9b17058b4a3
beauchama <abeauchamp@pulsewave.dev>
2023-09-24 19:14:12 -0400

    Added samples and fixture to read country json

---
+++ tests/FlagQuest.Migrator.Tests/Samples/ICountryData.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace FlagQuest.Migrator.Samples;
+
+public interface ICountryData
+{
+    string Common { get; }
+
+    string Official { get; }
+
+    string CountryCode3 { get; }
+
+    IEnumerable<string> Capitals { get; }
+
+    string Region { get; }
+
+    string SubRegion { get; }
+
+    IEnumerable<string> Languages { get; }
+
+    string FrenchCommon { get; }
+
+    string FrenchOfficial { get; }
+
+    double Latitude { get; }
+
+    double Longitude { get; }
+
+    bool Landlocked { get; }
+
+    IEnumerable<string> Borders { get; }
+
+    double Area { get; }
+}
---
+++ tests/FlagQuest.Migrator.Tests/Samples/Italy.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace FlagQuest.Migrator.Samples;
+
+public sealed record Italy : ICountryData
+{
+    public string Common { get; } = "Italy";
+
+    public string Official { get; } = "Italian Republic";
+
+    public string CountryCode3 { get; } = "ITA";
+
+    public IEnumerable<string> Capitals { get; } = ["Rome"];
+
+    public string Region { get; } = "Europe";
+
+    public string SubRegion { get; } = "Southern Europe";
+
+    public IEnumerable<string> Languages { get; } = ["ita"];
+
+    public string FrenchCommon { get; } = "Italie";
+
+    public string FrenchOfficial { get; } = "République italienne";
+
+    public double Latitude { get; } = 42.83333333;
+
+    public double Longitude { get; } = 12.83333333;
+
+    public bool Landlocked { get; }
+
+    public IEnumerable<string> Borders { get; } = ["AUT", "FRA", "SMR", "SVN", "CHE", "VAT"];
+
+    public double Area { get; } = 301336;
+}
