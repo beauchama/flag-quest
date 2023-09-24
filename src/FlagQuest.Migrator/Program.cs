a41a795305be34bdbe6d5c8cb1a7b2b02a29476a
beauchama <abeauchamp@pulsewave.dev>
2023-09-24 15:33:56 -0400

    Added FlagQuest.Migrator and his test project

---
+++ src/FlagQuest.Migrator/Program.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+string helloWorld = "Hello, world!";
+
+Console.WriteLine(helloWorld);
