86ccb0f9521c801a89986a4ab32713767040e2e8
beauchama <abeauchamp@pulsewave.dev>
2023-09-24 20:58:19 -0400

    Added CountryHttpClient

---
+++ src/FlagQuest.Migrator/Clients/CountryHttpClient.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+using FlagQuest.Migrator.Configurations;
+using FlagQuest.Migrator.Entities;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace FlagQuest.Migrator.Clients;
+
+internal sealed class CountryHttpClient(IOptions<DataSourceOptions> dataSourceOptions, IOptions<JsonOptions> jsonOptions, HttpClient client) : ICountryHttpClient
+{
+    public async Task<IReadOnlyCollection<Country>> GetAsync()
+    {
+        JsonSerializerOptions options = new()
+        {
+            PropertyNameCaseInsensitive = jsonOptions.Value.CaseInsensitive,
+            WriteIndented = jsonOptions.Value.Minify
+        };
+
+        using HttpResponseMessage response = await client.GetAsync(dataSourceOptions.Value.Countries).ConfigureAwait(false);
+
+        return (await response.Content.ReadFromJsonAsync<Country[]>(options).ConfigureAwait(false))!;
+    }
+}
---
+++ src/FlagQuest.Migrator/Clients/ICountryHttpClient.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+using FlagQuest.Migrator.Entities;
+
+namespace FlagQuest.Migrator.Clients;
+
+internal interface ICountryHttpClient
+{
+    Task<IReadOnlyCollection<Country>> GetAsync();
+}
