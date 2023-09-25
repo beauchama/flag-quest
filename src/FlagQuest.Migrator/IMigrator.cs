5239812351812e9cb0772accc3a4aef84d9fd35f
beauchama <abeauchamp@pulsewave.dev>
2023-09-24 20:58:41 -0400

    Added Migrator for Country

---
+++ src/FlagQuest.Migrator/IMigrator.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace FlagQuest.Migrator;
+
+internal interface IMigrator
+{
+    Task MigrateAsync(string path);
+}
