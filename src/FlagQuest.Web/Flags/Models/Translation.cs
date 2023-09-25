a5a1fae8eb31bca7445ba66597fee5bca0861f1f
beauchama <abeauchamp@pulsewave.dev>
2023-09-24 23:32:54 -0400

    Added Flag model

---
+++ src/FlagQuest.Web/Flags/Models/Translation.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace FlagQuest.Web.Flags.Models;
+
+public sealed record Translation(string Name, string OfficialName);
---
+++ src/FlagQuest.Web/Flags/State/Mappers/TranslationMapper.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace FlagQuest.Web.Flags.State.Mappers;
+
+internal static class TranslationMapper
+{
+    internal static Models.Translations ToTranslations(this Api.Resources.Translations translations)
+        => new(translations.English.ToTranslation(), translations.French.ToTranslation());
+
+    private static Models.Translation ToTranslation(this Api.Resources.Translation translation)
+        => new(translation.Name, translation.OfficialName);
+}
