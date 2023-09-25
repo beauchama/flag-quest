d982ee18f2aff7ccef79425e2c4b211951c16509
beauchama <abeauchamp@pulsewave.dev>
2023-09-24 20:57:15 -0400

    Added FlagQuest.Api

---
+++ src/FlagQuest.Api/Resources/Translations.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+using System.Text.Json.Serialization;
+
+namespace FlagQuest.Api.Resources;
+
+/// <summary>
+/// Represents a list of translations.
+/// </summary>
+public sealed record Translations
+{
+    /// <summary>
+    /// Gets the French translation.
+    /// </summary>
+    [JsonPropertyName("fra")]
+    public required Translation French { get; init; }
+
+    /// <summary>
+    /// Gets the English translation.
+    /// </summary>
+    [JsonPropertyName("eng")]
+    public required Translation English { get; init; }
+}
