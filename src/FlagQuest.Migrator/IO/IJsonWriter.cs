5dbf6c96343f195236f57ddd819dbd9946b2c564
beauchama <abeauchamp@pulsewave.dev>
2023-09-24 20:58:30 -0400

    Added JsonWriter

---
+++ src/FlagQuest.Migrator/IO/IJsonWriter.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace FlagQuest.Migrator.IO;
+
+internal interface IJsonWriter
+{
+    Task WriteAsync<T>(string path, T value);
+}
