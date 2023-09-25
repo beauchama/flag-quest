97855c54610101c206ffd82ab2661650182fe91f
beauchama <abeauchamp@pulsewave.dev>
2023-09-24 22:00:16 -0400

    Added FlagQuest.Web

---
+++ src/FlagQuest.Web/Program.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+using FlagQuest.Web;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync().ConfigureAwait(false);
