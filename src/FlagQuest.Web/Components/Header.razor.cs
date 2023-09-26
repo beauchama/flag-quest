06f15f1250e486ffbed6fbf134ba40dfa4f0fb05
beauchama <abeauchamp@pulsewave.dev>
2023-09-26 00:54:52 -0400

    Added Header

---
+++ src/FlagQuest.Web/Components/Header.razor.cs
@@ -0,0 +1,20 @@
+// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+using FlagQuest.Web.Configurations;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Options;
+
+namespace FlagQuest.Web.Components;
+
+public partial class Header
+{
+    [Inject]
+    private IOptions<GitHubOptions> GitHubOptions { get; set; } = default!;
+
+    private static void OnQuestionClick() => Console.WriteLine("Question?");
+
+    private static void OnStatsClick() => Console.WriteLine("Stats");
+
+    private static void OnSettingsClick() => Console.WriteLine("Settings");
+}
