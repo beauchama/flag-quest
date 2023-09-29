c520a84707402e3f253a4ffa19b137431ab1b101
beauchama <abeauchamp@pulsewave.dev>
2023-09-29 00:12:10 -0400

    First release of FlagQuest

--- src/FlagQuest.Web/wwwroot/js/rotate-arrow.js
+++ src/FlagQuest.Web/wwwroot/js/rotate-arrow.js
@@ -1,20 +1,13 @@
 function rotateArrow(code, guessedCoordinate, coordinate) {
     const arrow = document.getElementById(code).getElementsByClassName('arrow')[0];
 
-    console.log(code);
-    console.log(guessedCoordinate);
-    console.log(coordinate);
-
     const angle = calculateAngle();
-    console.log(angle);
+
     arrow.style.transform = `rotate(${angle - 90}deg)`;
 
     function calculateAngle() {
         const latitude = coordinate.latitude - guessedCoordinate.latitude
         const longitude = coordinate.longitude - guessedCoordinate.longitude
-
-        console.log(latitude);
-        console.log(longitude);
 
         return Math.atan2(longitude, latitude) * 180 / Math.PI;
     }
